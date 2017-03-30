8decdac7105540448fa80d12025ad9f7f001c344
antoineADB38 <di-bartolomeo.antoine@orange.fr>
2017-03-30 23:12:59 +0200

    version centrale de proximité phase 1

--- class/Partenairefromprojet.php
+++ class/Partenairefromprojet.php
@@ -4,9 +4,9 @@ class Partenairefromprojet {
 
     private $_centralepartenaireprojet;
     private $_partenaire1;
-
+    
     public function __construct($centralepartenaireprojet, $partenaire1) {
-        $this->setCentralepartenaireprojet($centralepartenaireprojet);
+        $this->setCentralepartenaireprojet($centralepartenaireprojet);        
         $this->setPartenaire1($partenaire1);
     }
 
@@ -16,11 +16,11 @@ class Partenairefromprojet {
 
     public function setCentralepartenaireprojet($centralepartenaireprojet) {
         $this->_centralepartenaireprojet = $centralepartenaireprojet;
-    }
+    }    
 
     public function getPartenaire1() {
         return $this->_partenaire1;
-    }
+}
 
     public function setPartenaire1($param) {
         $this->_partenaire1 = $param;
